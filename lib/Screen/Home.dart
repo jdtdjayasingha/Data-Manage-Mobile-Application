bd0db20b9f7cb5a8f09057ace7290799e16c3a7f
Tharindu Dilshan <153268657+jdtdjayasingha@users.noreply.github.com>
2024-02-05 22:56:41 +0530

    Created the screens

---
+++ lib/Screen/Home.dart
@@ -0,0 +1,55 @@
+import 'package:data_manage_mobile_application/Screen/NewStudent.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => NewStudent(),
+              ));
+        },
+        child: Icon(Icons.add),
+      ),
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              "Student ",
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 30,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Text(
+              "Note",
+              style: TextStyle(
+                color: Colors.yellow,
+                fontSize: 30,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
